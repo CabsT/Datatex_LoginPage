898903dcc739223cbedc5adf24e07da53360cd9a
CabsT <133862761+CabsT@users.noreply.github.com>
2024-02-25 10:41:31 +0200

    Spelling mistake corrected

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -39,7 +39,7 @@ export default function LoginPage() {
   }
 
   function SuccessMessage() {
-    alert("Login submitted succesfully");
+    alert("Login submitted successfully");
   }
 
   let form = (
